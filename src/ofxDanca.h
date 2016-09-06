fd925db68c263c4aad55b45ee651ff8f4204b1c5
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-09-06 21:30:47 +0200

    Draw preliminary Vision layer

--- src/ofxDanca.h
+++ src/ofxDanca.h
@@ -1,21 +1,28 @@
 #pragma once
 
 #include "Camera.h"
+#include "Vision.h"
+
+// TODO: consider SharedData object
 
 class ofxDanca {
 	public:
 		void setup(){
 			_camera.setup();
+			_vision.setup(&_camera);
 		}
 	
 		void update(){
 			_camera.update();
+			_vision.update(&_camera);
 		}
 	
 		void draw(){
 			_camera.draw();
+			_vision.draw(&_camera);
 		}
 	
 	private:
 		ofx::danca::Camera _camera;
+		ofx::danca::Vision _vision;
 };
