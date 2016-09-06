d9819e24387d0cd70b94eca74dcc924e41c1c199
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-09-06 21:29:30 +0200

    Improve Camera, add public methods
    - Add getWidth and getHeight getters
    - Add getFrame getter

--- src/Camera.h
+++ src/Camera.h
@@ -14,19 +14,17 @@ namespace danca {
 class Camera {
     public:
         Camera(){
-		
+			_width = 480;
+			_height = 360;
 		}
     
         void setup(){
-			int w = 480;
-			int h = 360;
-
 			#ifdef TARGET_RASPBERRY_PI
-				_camera.setup(w, h, false);
+				_camera.setup(_width, _height, false);
 			#else
 				_camera.setDeviceID(0);
 				_camera.setDesiredFrameRate(60);
-				_camera.initGrabber(w, h);
+				_camera.initGrabber(_width, _height);
 			#endif
 		}
 	
@@ -49,7 +47,22 @@ class Camera {
 			ofxCv::drawMat(_frame, 0, 0, ofGetWidth(), ofGetHeight());
 		}
 	
+		int getWidth(){
+			return _width;
+		}
+	
+		int getHeight(){
+			return _height;
+		}
+	
+		cv::Mat & getFrame(){
+			return _frame;
+		}
+	
 	private:
+		int _width;
+		int _height;
+	
 		#ifdef TARGET_RASPBERRY_PI
             ofxCvPiCam _camera;
         #else
